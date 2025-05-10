c7421a99cf0f9078cdb065d11bc175b8c43c59b5
mdk <brookside4@gmail.com>
2025-05-10 09:43:27 -0500

    update existing postcss installation. Update postcss.config.js file

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,15 @@
 const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./hugo_stats.json"],
+  content: ["./hugo_stats.json"], // Path to Hugo's stats file
   defaultExtractor: (content) => {
     const els = JSON.parse(content).htmlElements;
     return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
   },
-  safelist: [],
+  safelist: [], // Add any classes you want to keep
 });
 
 module.exports = {
   plugins: [
-    ...(process.env.HUGO_ENVIRONMENT === "production" ? [purgecss] : []),
+    require("autoprefixer"), // Adds vendor prefixes for cross-browser compatibility
+    ...(process.env.HUGO_ENVIRONMENT === "production" ? [purgecss, require("cssnano")] : []), // Purge and minify only in production
   ],
 };
